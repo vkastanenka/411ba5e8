d2acb0125af67339014210b846e9b29d014792c8
Victoria Kastanenka <vkastanenka@gmail.com>
2024-12-03 18:26:33 -0500

    updated modal

--- src/components/activity-call-card.tsx
+++ src/components/activity-call-card.tsx
@@ -76,35 +76,37 @@ export const ActivityCardModal: React.FC<ActivityCardModalProps> = ({
 }) => {
   return (
     <Dialog open={isModalOpen} onOpenChange={(e) => setIsModalOpen(e)}>
-      <DialogContent className="bg-card flex flex-col gap-4 min-h-[394px]">
+      <DialogContent className="bg-card flex flex-col gap-4">
         <DialogTitle>
           {`${call.call_type === 'answered' ? 'Call' : 'Missed call'} ${
             call.direction === 'outbound' ? 'to' : 'from'
           } ${call.from}`}
         </DialogTitle>
-        <DialogDescription>Call details</DialogDescription>
-        <div className="flex flex-col gap-4">
+        <DialogDescription className="text-base">
+          Call details
+        </DialogDescription>
+        <div className="flex flex-col gap-4 [&>div]:flex [&>div]:items-center [&>div]:gap-1">
           <div>
-            <p className="text-[14px]">Call made:</p>
-            <p className="text-[14px]">
+            <p className="text-sm">Call made:</p>
+            <p className="text-sm">
               {formatDate(parseISO(call.created_at), 'MMM dd yyyy, h:mm aaa')}
             </p>
           </div>
           <div>
-            <p className="text-[14px]">Call type:</p>
-            <p className="text-[14px]">{call.call_type}</p>
+            <p className="text-sm">Call type:</p>
+            <p className="text-sm">{call.call_type}</p>
           </div>
           <div>
-            <p className="text-[14px]">Call direction:</p>
-            <p className="text-[14px]">{call.direction}</p>
+            <p className="text-sm">Call direction:</p>
+            <p className="text-sm">{call.direction}</p>
           </div>
           <div>
-            <p className="text-[14px]">Call duration:</p>
-            <p className="text-[14px]">{call.duration}</p>
+            <p className="text-sm">Call duration:</p>
+            <p className="text-sm">{call.duration}</p>
           </div>
           <div>
-            <p className="text-[14px]">Aircall number:</p>
-            <p className="text-[14px]">{call.via}</p>
+            <p className="text-sm">Aircall number:</p>
+            <p className="text-sm">{call.via}</p>
           </div>
           <ArchiveButton
             calls={[call]}
